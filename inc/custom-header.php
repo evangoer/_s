874b92c605d3ddcbd40940ab218770a12642db92
Evan Goer <evan@goer.org>
2013-11-22 21:48:35 -0800

    Rename internal _s references to "pure"

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -12,39 +12,39 @@
 	<?php endif; // End header image check. ?>
 
  *
- * @package _s
+ * @package pure
  */
 
 /**
  * Setup the WordPress core custom header feature.
  *
- * @uses _s_header_style()
- * @uses _s_admin_header_style()
- * @uses _s_admin_header_image()
+ * @uses pure_header_style()
+ * @uses pure_admin_header_style()
+ * @uses pure_admin_header_image()
  *
- * @package _s
+ * @package pure
  */
-function _s_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( '_s_custom_header_args', array(
+function pure_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'pure_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => '_s_header_style',
-		'admin-head-callback'    => '_s_admin_header_style',
-		'admin-preview-callback' => '_s_admin_header_image',
+		'wp-head-callback'       => 'pure_header_style',
+		'admin-head-callback'    => 'pure_admin_header_style',
+		'admin-preview-callback' => 'pure_admin_header_image',
 	) ) );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', 'pure_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( 'pure_header_style' ) ) :
 /**
  * Styles the header image and text displayed on the blog
  *
- * @see _s_custom_header_setup().
+ * @see pure_custom_header_setup().
  */
-function _s_header_style() {
+function pure_header_style() {
 	$header_text_color = get_header_textcolor();
 
 	// If no custom options for text are set, let's bail
@@ -76,15 +76,15 @@ function _s_header_style() {
 	</style>
 	<?php
 }
-endif; // _s_header_style
+endif; // pure_header_style
 
-if ( ! function_exists( '_s_admin_header_style' ) ) :
+if ( ! function_exists( 'pure_admin_header_style' ) ) :
 /**
  * Styles the header image displayed on the Appearance > Header admin panel.
  *
- * @see _s_custom_header_setup().
+ * @see pure_custom_header_setup().
  */
-function _s_admin_header_style() {
+function pure_admin_header_style() {
 ?>
 	<style type="text/css">
 		.appearance_page_custom-header #headimg {
@@ -104,15 +104,15 @@ function _s_admin_header_style() {
 	</style>
 <?php
 }
-endif; // _s_admin_header_style
+endif; // pure_admin_header_style
 
-if ( ! function_exists( '_s_admin_header_image' ) ) :
+if ( ! function_exists( 'pure_admin_header_image' ) ) :
 /**
  * Custom header image markup displayed on the Appearance > Header admin panel.
  *
- * @see _s_custom_header_setup().
+ * @see pure_custom_header_setup().
  */
-function _s_admin_header_image() {
+function pure_admin_header_image() {
 	$style = sprintf( ' style="color:#%s;"', get_header_textcolor() );
 ?>
 	<div id="headimg">
@@ -124,4 +124,4 @@ function _s_admin_header_image() {
 	</div>
 <?php
 }
-endif; // _s_admin_header_image
+endif; // pure_admin_header_image
--- searchform.php
+++ searchform.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * The template for displaying search forms in _s
+ * The template for displaying search forms in pure
  *
- * @package _s
+ * @package pure
  */
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', '_s' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_s' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'pure' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'pure' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', '_s' ); ?>">
+	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'pure' ); ?>">
 </form>
