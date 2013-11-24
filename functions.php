1534269ba5a8a5a9b58d9988f1432f7de9932ae3
Evan Goer <evan@goer.org>
2013-11-23 23:28:28 -0800

    Update heading markup & style

--- functions.php
+++ functions.php
@@ -65,8 +65,8 @@ function pure_widgets_init() {
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'pure_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -14,14 +14,14 @@
 			</aside>
 
 			<aside id="archives" class="widget">
-				<h1 class="widget-title"><?php _e( 'Archives', 'pure' ); ?></h1>
+				<h2 class="widget-title"><?php _e( 'Archives', 'pure' ); ?></h2>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</aside>
 
 			<aside id="meta" class="widget">
-				<h1 class="widget-title"><?php _e( 'Meta', 'pure' ); ?></h1>
+				<h2 class="widget-title"><?php _e( 'Meta', 'pure' ); ?></h2>
 				<ul>
 					<?php wp_register(); ?>
 					<li><?php wp_loginout(); ?></li>
