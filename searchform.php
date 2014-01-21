f67b110b6d27387b97bb85d355fac5ef9c963fd5
Evan Goer <evan@goer.org>
2014-01-20 22:34:54 -0800

    Center search & clarify meaning

--- searchform.php
+++ searchform.php
@@ -6,6 +6,6 @@
  */
 ?>
 <form role="search" method="get" class="pure-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" aria-label="<?php _ex( 'Search for:', 'label', 'pure' ); ?>" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'pure' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+	<input type="search" aria-label="<?php _ex( 'Search for:', 'label', 'pure' ); ?>" placeholder="<?php echo esc_attr_x( 'Search this site &hellip;', 'placeholder', 'pure' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	<input type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'pure' ); ?>">
 </form>
